56650ac398b235b5c2474ad4351b599b7f2aa09a
Anthony Waddell <awad34@uw.edu>
2019-06-01 14:40:37 -0700

    changed header and list_customer to list_missing_children. commented out list_product

--- header.inc.php
+++ header.inc.php
@@ -2,15 +2,16 @@
 /**
  * Created by PhpStorm.
  * User: MKochanski
- * Date: 7/24/2018
- * Time: 3:37 PM
+ * Edited by Team 4
+ * Date: 6-1-19
+ * Time: 2:24 PM
  */
 ?>
 <!-- This is a common navigation base to call different web pages on the website -->
 <div class="topnav">
     <a href="index.php">Home</a>
-    <a href="list_customers.php">List Customers</a>
-    <a href="list_products.php">List Products</a>
+    <a href="list_missing_children.php">List Missing Children</a>
+    //<a href="list_products.php">List Products</a>
 </div></div>
 <h1>Sample PHP Database Program</h1>
 
